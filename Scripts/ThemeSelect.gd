5b8ae5b9791160987a1385f1589ee82e2882a2d1
MintStudios <mintstudios.dev@gmail.com>
2020-06-07 15:40:07 -0400

    Add themes option

---
+++ Scripts/ThemeSelect.gd
@@ -0,0 +1,19 @@
+extends OptionButton
+
+export var themes = [
+	"res://Themes/godot_theme.tres",
+	"res://Themes/solarized_theme.tres"
+]
+
+func _ready():
+	add_item("Godot", 0)
+	add_item("Solarized", 1)
+	
+	get_tree().root.get_child(0).theme = load(themes[0])
+
+func theme_select(id):
+	get_tree().root.get_child(0).theme = load(themes[id])
+	if get_tree().root.get_child(0).theme == load(themes[1]):
+		get_node('/root/Main/ColorRect').color = Color('#dcd2bb')
+	if get_tree().root.get_child(0).theme == load(themes[0]):
+		get_node('/root/Main/ColorRect').color = Color('#191a2f')
