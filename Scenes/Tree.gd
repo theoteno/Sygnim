26145a9c45c12b8b6660110cc45733711695d092
superRaptor911 <raptor.inc2018@gmail.com>
2020-09-04 22:19:14 +0530

    File explorer

--- Scenes/Tree.gd
+++ Scenes/Tree.gd
@@ -1,12 +1,15 @@
 extends Tree
 
-
+# icons, need more icons for various file types
 var icons = {
 	folder = preload("res://Resources/icons/icons8-folder-26.png"),
+	gd = preload("res://Resources/icons/icons8-gear-26.png"),
 	txt = preload("res://Resources/icons/icons8-txt-26.png")
 }
 
+# Current directory
 var base_dir = ""
+# Tree item root
 var root_dir = null
 
 func _ready():
@@ -19,20 +22,22 @@ func _ready():
 		var cur_file = GlobalData.settings.last_opened_files[count - 1]
 		setup_dir(cur_file.get_base_dir())
 
-
+# Load directory of file selected by "file selector" (present in main)
 func _on_file_selected(file_path : String):
 	var dir_path = file_path.get_base_dir()
 	setup_dir(dir_path)
 
-
-func get_end_directory_name(dir_path : String) -> String:
+# Get name of directory
+func get_directory_name(dir_path : String) -> String:
 	var strings = dir_path.split("/")
 	if strings.size() > 0:
 		return strings[strings.size() - 1]
 	return "/"
 
 
+# Setup ...
 func setup_dir(dir_path : String):
+	# Clear prev listing
 	if root_dir != null:
 		yield(get_tree(), "idle_frame")
 		root_dir.free()
@@ -46,8 +51,8 @@ func setup_dir(dir_path : String):
 	
 	var file_name = dir.get_next()
 	root_dir = create_item()
-	root_dir.set_text(0, get_end_directory_name(dir_path))
-
+	root_dir.set_text(0, get_directory_name(dir_path))
+	# Get dir contents
 	while file_name != "":
 		if file_name != ".":
 			if dir.current_is_dir():
@@ -55,7 +60,8 @@ func setup_dir(dir_path : String):
 			else:
 				files.append(file_name)
 		file_name = dir.get_next()
-
+	
+	# Sort them
 	dir_list.sort()
 	files.sort()
 	
@@ -67,8 +73,12 @@ func setup_dir(dir_path : String):
 	for i in files:
 		var item = create_item(root_dir)
 		item.set_text(0, i)
-		item.set_icon(0, icons.txt)
-
+		var ext = i.get_extension()
+		# Set appropriate icon if exists
+		if icons.has(ext):
+			item.set_icon(0, icons.get(ext))
+		else:
+			item.set_icon(0, icons.txt)
 
 
 func _on_Tree_item_selected():
@@ -80,6 +90,7 @@ func _on_Tree_item_selected():
 	else:
 		var path = base_dir + "/" + selected_file
 		var file : File = File.new()
+		# Chk file is folder or directory
 		if file.file_exists(path):
 			var main_scene = get_tree().get_nodes_in_group("Main")[0]
 			main_scene.open_file_selected(path)
